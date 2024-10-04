9f1b89a2c9e6ca1b2f85cc89d9ba499010b96dbd
Shrey <163460041+Shrey-Sawant@users.noreply.github.com>
2024-10-04 23:41:03 +0530

    REgister complete

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,29 +1,34 @@
 import { v2 as cloudinary} from "cloudinary";
 import fs from "fs";
+import dotenv from "dotenv";
 
+dotenv.config();
 
-   
     cloudinary.config({ 
         cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
         api_key: process.env.CLOUDINARY_API_KEY, 
-        api_secret:process.env.CLOUDINARY_API_SECRET 
+        api_secret:process.env.CLOUDINARY_API_SECRET ,
     });
     
 const uploadOnCloudinary=async (loaclFilePath)=>{
     try{
-        if(!loaclFilePath) return null
-
-       const response= await cloudinary.uploader.upload(loaclFilePath,{
+        if(!loaclFilePath) return null;
+        
+        const response= await cloudinary.uploader.upload(loaclFilePath,{
             resource_type:"auto"
-        })
+             });
 
-        console.log("file is uploaded on cloudinary\n\n "+response)
+        // console.log("file is uploaded on cloudinary\n\n "+response)
+        fs.unlinkSync(loaclFilePath);
         return response;
 
     }catch(error){
         fs.unlinkSync(loaclFilePath) //remove the locally savedd temp file
+        console.log(error)
         return null;
+
     }
 }
+
 
 export {uploadOnCloudinary};
