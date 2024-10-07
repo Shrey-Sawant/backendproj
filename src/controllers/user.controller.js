26567a09df4e5f32bbe07627ea3fdac316f519d9
Shrey <163460041+Shrey-Sawant@users.noreply.github.com>
2024-10-07 20:34:38 +0530

    Add refresh token endpoint

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,6 +3,23 @@ import { ApiError } from "../utils/ApiError.js"
 import { User } from "../models/user.models.js"
 import { uploadOnCloudinary } from "../utils/cloudinary.js"
 import { ApiResponse } from "../utils/ApiResponse.js"
+import jwt from "jsonwebtoken";
+
+const generateAccessAndREfreshTokens = async (userId,) => {
+    try {
+        const user = await User.findById(userId);
+        const accessToken = user.generateAccessToken();
+        const refreshToken = user.generateRefershToken();
+
+        user.refreshToken = refreshToken;
+        await user.save({ validateBeforeSave: false });
+
+        return { accessToken, refreshToken };
+
+    } catch (error) {
+        throw new ApiError(500, "Something went wrong while generating refresh and access token")
+    }
+}
 
 const registerUser = asyncHandler(async (req, res) => {
 
@@ -28,14 +45,14 @@ const registerUser = asyncHandler(async (req, res) => {
     // const coverImageLocalPath = req.files?.coverImage[0]?.path;
 
     let coverImageLocalPath;
-    if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length >0){
-        coverImageLocalPath=req.files.coverImage[0].path;
+    if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
+        coverImageLocalPath = req.files.coverImage[0].path;
     }
     if (!avatarLocalPath) {
         throw new ApiError(400, " Avatar file is required");
     }
 
-    const  avatar = await uploadOnCloudinary(avatarLocalPath);
+    const avatar = await uploadOnCloudinary(avatarLocalPath);
     const coverImage = await uploadOnCloudinary(coverImageLocalPath)
 
     console.log(avatar);
@@ -45,7 +62,7 @@ const registerUser = asyncHandler(async (req, res) => {
 
     const user = await User.create({
         fullName,
-        avatar:  avatar.url,
+        avatar: avatar.url,
         coverImage: coverImage?.url || "",
         email,
         password,
@@ -53,7 +70,7 @@ const registerUser = asyncHandler(async (req, res) => {
     })
 
     const createddUser = await User.findById(user._id).select(
-        "-password -refreshTokenn"
+        "-password -refreshToken"
     )
 
     if (!createddUser) {
@@ -66,4 +83,116 @@ const registerUser = asyncHandler(async (req, res) => {
 
 })
 
-export { registerUser };    
+const loginUser = asyncHandler(async (req, res) => {
+    const { email, userName, password } = req.body;
+
+    if (!(userName || email)) {
+        throw new ApiError(400, "Username and Email is required");
+    }
+
+    const user = User.findOne({
+        $or: [{ userName }, { email }]
+    })
+
+    if (!user) {
+        throw new ApiError(404, "User does not Exits")
+    }
+
+    const isPasswordValid = await user.isPasswordCorrect(password);
+    if (!isPasswordValid) {
+        throw new ApiError(401, "Invalid User Credentials");
+    }
+
+    const { accessToken, refreshToken } = await generateAccessAndREfreshTokens(user._id);
+
+    const loggedInUser = awaitUser.findById(user._id).select("-password -refreshToken");
+
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+
+    return res
+        .status(200)
+        .cookie("accessToken", accessToken, options)
+        .cookie("refreshToken", refreshToken, options)
+        .json(
+            new ApiResponse(
+                200,
+                {
+                    user: loggedInUser, accessToken,
+                    refreshToken
+                },
+                "User Logged In Successfully")
+        )
+
+})
+
+const logoutUser = asyncHandler(async (req, res) => {
+    await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $set: {
+                refreshToken: undefined
+            }
+        },
+        {
+            new: true
+        }
+    )
+
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+
+    return res
+        .status(200)
+        .clearCookie("accessToken", options)
+        .clearCookie("refreshToken", options)
+        .json(new ApiResponse(200, {}, "User Loggedout Successfully"));
+})
+
+const refreshAccessToken = asyncHandler(async (req, res) => {
+    try {
+        const incomingRefreshToken = req.cookies.refreshToken || req.body.refreshToken;
+    
+        if (!incomingRefreshToken) {
+            throw new ApiError(401, "Unaouthorized request");
+        }
+    
+        const decodedToken = jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET);
+    
+        const user = await User.findById(decodedToken?._id);
+    
+        if (!user) {
+            throw new ApiError(401, "Invalid Refresh Token");
+        }
+    
+        if(incomingRefreshToken!=user?.refreshToken){
+            throw new ApiError("Refresh TOken is expired or used");
+        }
+    
+        const options={
+            httpOnly:true,
+            secure:true
+        }
+    
+        const {accessToken,newRefreshToken} =await generateAccessAndREfreshTokens(user._id);
+    
+        return res
+               .status(200)
+               .cookie("accessToken",accessToken,options)
+               .cookie("refreshToken",newRefreshToken,options)
+               .json(
+                new ApiResponse(
+                    200,
+                    {accessToken,refreshToken:newRefreshToken,},
+                    "Access Token Refreshed"
+                )
+               )
+    } catch (error) {
+        throw new ApiError(401,error?.message || "Invalid Refersh Token")
+    }
+})
+export { registerUser, loginUser, logoutUser,refreshAccessToken };
--- src/db/index.js
+++ src/db/index.js
@@ -6,8 +6,8 @@ const connectDB=async ()=>{
        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
        console.log(`\nMongoDB Connected!!! DB Host:${connectionInstance.connection.host}`);
     } catch (error) {
-        console.groupCollapsed("Error: MONGODB CONNECTION ERROR:",error)
-        process.exit(1)
+        console.log("Error: MONGODB CONNECTION ERROR:",error);
+        process.exit(1);
     }
 }
 
